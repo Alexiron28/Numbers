66e9720e714bc9ee062e554eeaf432f02e02f243
Alexiron28 <99764698+Alexiron28@users.noreply.github.com>
2022-02-15 14:47:06 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Numeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int a;
+            for (a = 1; a < 101; a++)
+            {
+                if(a%3==0)
+                {
+                    Console.WriteLine("Ordico");
+                }
+                if(a%5==0)
+                {
+                    Console.WriteLine("OrdicoN");
+                }
+                if (a % 5 == 0 && a % 3 == 0)
+                {
+                    Console.WriteLine("OrdicoX");
+                }
+                else{ 
+                Console.WriteLine(a);
+                }
+            }
+        }
+    }
+}
